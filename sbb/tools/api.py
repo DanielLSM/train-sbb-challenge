c5b3e8da4d3f97206e756757e2952d4e7f538aac
daniel <daniellsmarta@gmail.com>
2018-09-21 11:41:44 +0100

    changes on the instance API

--- sbb/tools/api.py
+++ sbb/tools/api.py
@@ -1,9 +1,19 @@
+import logging
 from sbb.tools.parsers import parse_input_paths, parse_json_file
+from sbb.tools import input_dir
 
 
-class InstanceAPI(object):
+class InstanceAPI:
 
-    def __init__(self):
-        pass
+    # int: instance_number
 
-    pass
+    # import pdb
+    # pdb.set_trace()
+
+    def __init__(self, ninstance: int = 0, input_dir: str = input_dir):
+        self._ninstance = ninstance
+        self._input_dir = input_dir
+        self.ipaths = parse_input_paths(input_dir)
+        self.api_logger = logging.getLogger('APIloger')
+        self.api_logger.setLevel(logging.DEBUG)
+        self.api_logger.info('API for the Instances Initialized')
