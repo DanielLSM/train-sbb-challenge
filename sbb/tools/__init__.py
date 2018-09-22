2862ccb528d1eb431d7521e346099eb5196f2a7c
daniel <daniellsmarta@gmail.com>
2018-09-22 15:44:15 +0100

    api more visually friendly

--- sbb/tools/__init__.py
+++ sbb/tools/__init__.py
@@ -1,1 +1,2 @@
 input_dir="../../../../usr/train-schedule-optimisation-challenge-starter-kit/problem_instances"
+input_samples="../../../../usr/train-schedule-optimisation-challenge-starter-kit/sample_files"
--- sbb/tools/api.py
+++ sbb/tools/api.py
@@ -3,12 +3,12 @@ import sbb.tools.logger
 import pprint
 
 from sbb.tools.parsers import parse_input_paths, parse_json_file
-from sbb.tools import input_dir
+from sbb.tools import input_dir, input_samples
 
 
 class InstanceAPI:
 
-    def __init__(self, ninstance: int = 0, input_dir: str = input_dir):
+    def __init__(self, ninstance: int = 0, input_dir: str = input_samples):
         self._input_dir = input_dir
         self._ipaths = parse_input_paths(input_dir)
 
@@ -22,14 +22,20 @@ class InstanceAPI:
     def __str__(self):
         return 'API to interface instance {}'.format(self._fname)
 
+    def keys(self):
+        return self.data.keys()
+
+    def __getitem__(self, key):
+        return pprint.pprint(self.data[key])
+
     def load_data(self, ninstance: int) -> dict:
         try:
             self.api_logger.info('loaded {}'.format(
                 self._ipaths[ninstance].parts[-1]))
             return parse_json_file(self._ipaths[ninstance])
         except ValueError as e:
-            self.api_logger.ERROR(
-                "Select an instance from 1 to {}".format(len(self._ipaths) + 1))
+            self.api_logger.ERROR("Select an instance from 0 to {}".format(
+                len(self._ipaths)))
             raise e
 
     # def inspect(self, key: str):
