0b7a5cfe1bc28f143a8d0616e4fff6f97e4b97fa
daniel <daniellsmarta@gmail.com>
2018-09-25 17:11:49 +0100

    update structure

---
+++ sbb/algos/models.py
@@ -0,0 +1,15 @@
+import logging
+import sbb.tools.logger
+
+from sbb.tools.api import InstanceAPI
+
+logger = logging.getLogger('APILogger')
+
+if __name__ == '__main__':
+    instance = InstanceAPI()
+    print(instance)
+    paths = instance.generate_all_paths(111)
+    a = paths[0][0]
+    b = paths[0][-1]
+    paths = instance.generate_paths(111, a, b)
+    paths1 = instance.generate_edge_paths(111, a, b)
